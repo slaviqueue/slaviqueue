8fb72ca6a31d7c33ea231b3818c1fba9acee32db
arf1e <anotheruselessbox@gmail.com>
2021-06-01 09:26:15 +0300

    tests

---
+++ test.js
@@ -0,0 +1,12 @@
+function testShrek() {
+  const shrek = 'shrek';
+  for (let i = 0; i < 1337; i++) {
+    console.log(`${shrek} is life`);
+    console.log(`${shrek} is love`);
+    console.log('===================');
+  }
+  console.log('TESTS SUCCESSFULLY PASSED');
+  return true;
+}
+
+testShrek();
